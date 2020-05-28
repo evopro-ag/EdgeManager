c866905311f1e94932af0c1a8d3df02c78c2d340
mdelicati <marco.delicati@evopro-ag.de>
2020-05-28 11:25:10 +0200

    created empty project

---
+++ EdgeManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EdgeManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
