9335e83ea913120eb93fff16c65dd2c090d6f4c0
mdelicati <marco.delicati@evopro-ag.de>
2020-05-29 14:14:19 +0200

    added some classes to AdgeManager.Gui

--- EdgeManager.Gui/MainWindow.xaml.cs
+++ EdgeManager.Gui/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Mime;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace EdgeManager.Gui
 {
@@ -23,6 +13,11 @@ namespace EdgeManager.Gui
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //this.SvCode.Content = EdgeManager.;
         }
     }
 }
---
+++ EdgeManager.Gui/ViewModelLocator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EdgeManager.Gui
+{
+    class ViewModelLocator
+    {
+    }
+}
