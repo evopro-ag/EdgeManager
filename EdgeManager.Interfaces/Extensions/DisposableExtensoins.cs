5504e429c319cd2bdfdc993b291b150b3d41eb4a
mdelicati <marco.delicati@evopro-ag.de>
2020-06-15 11:49:21 +0200

    Lost extension

---
+++ EdgeManager.Interfaces/Extensions/DisposableExtensoins.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdgeManager.Interfaces.Extensions
+{
+    public static class DisposableExtensoins
+    {
+        public static T AddDisposableTo<T>(this T disposable, CompositeDisposable disposables) where T : IDisposable
+        {
+            disposables.Add(disposable);
+            return disposable;
+        }
+    }
+}
