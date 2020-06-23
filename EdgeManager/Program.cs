a00cbcb072ed2a0f2a85f1714dc39a2ba43f9f56
mdelicati <marco.delicati@evopro-ag.de>
2020-06-23 16:32:15 +0200

    delete old codecomments

--- EdgeManager/Program.cs
+++ EdgeManager/Program.cs
@@ -59,22 +59,6 @@ namespace EdgeManager
                     LoggerFactory.GetLogger(typeof(Program)).Error("Unhandled exeption", e);
                 }
             }
-
-            //    LoadModules(kernel);
-
-            //    ////create azure clie
-            //    //            IAzureService azure = kernel.Get<IAzureService>();
-
-            //    //GetAllIoTHubInfo(azure).Wait();
-            //}
-
-            //using (var azure = new AzureCliHost())
-            //{
-            //var list = await azure.Run<IoTHubInfo[]>("iot hub list");
-
-            //await azure.CallMethod("ping", "evoproTestHub", "IoT_Edge_One", "$edgeAgent", new DirectMethodPayloadBase()).Dump();
-            //return;
-
         }
 
         private static Application CreateApplication(IViewModelFactory viewModelLocator)
@@ -93,30 +77,6 @@ namespace EdgeManager
             log4net.Config.XmlConfigurator.Configure(loggerRepository, new FileInfo("log4net.config"));
             
         }
-
-        //private static async Task GetAllIoTHubInfo(IAzureService azure)
-        //{
-        //    var list = await azure.GetIoTHubs();
-        //    Console.WriteLine(list);
-        //    foreach (var hub in list)
-        //    {
-        //        var deviceList = await azure.GetIoTDevices(hub.Name);
-        //        deviceList.Dump($"Devices for {hub.Name}");
-        //        foreach (var device in deviceList)
-        //        {
-        //            if (device.Capabilities.IoTEdge)
-        //            {
-        //                var moduleList = await azure.GetIoTModules(hub.Name, device.DeviceId);
-        //                moduleList.Dump($"Modules for {device.DeviceId}");
-        //                foreach (var modul in moduleList)
-        //                {
-        //                    var methodPayload = await azure.CallMethod("ping", hub.Name, device.DeviceId, modul.ModuleId, new DirectMethodPayloadBase());
-        //                    methodPayload.Dump($"Ping Status for {modul.ModuleId}");
-        //                }
-        //            }
-        //        }
-        //    }
-        //}
 
         private static void LoadModules(IKernel kernel)
         {
