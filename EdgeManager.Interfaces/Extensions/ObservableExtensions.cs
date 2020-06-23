dfff752e3fb1d1f45ad9cb831cc4d295096ce02a
mdelicati <marco.delicati@evopro-ag.de>
2020-06-23 13:50:39 +0200

    added obs extensions

---
+++ EdgeManager.Interfaces/Extensions/ObservableExtensions.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using log4net;
+
+namespace EdgeManager.Interfaces.Extensions
+{
+	public static class ObservableExtensions
+	{
+		public static IObservable<T> LogAndRetry<T>(this IObservable<T> source, ILog logger, string message)
+		{
+			return source
+				.Do(
+					_ => { },
+					ex => logger?.Error(message, ex))
+				.Retry();
+		}
+
+		public static IObservable<T> RetryAfterDelay<T>(
+			this IObservable<T> source,
+			TimeSpan retryDelay)
+		{
+			return RedoAfterDelay(source, retryDelay, -1, null, Observable.Retry, Observable.Retry);
+		}
+
+		public static IObservable<T> RetryAfterDelay<T>(
+			this IObservable<T> source,
+			TimeSpan retryDelay,
+			int retryCount,
+			IScheduler scheduler)
+		{
+			return RedoAfterDelay(source, retryDelay, retryCount, scheduler, Observable.Retry, Observable.Retry);
+		}
+
+		public static IObservable<T> RepeatAfterDelay<T>(
+			this IObservable<T> source,
+			TimeSpan retryDelay)
+		{
+			return RedoAfterDelay(source, retryDelay, -1, null, Observable.Repeat, Observable.Repeat);
+		}
+		public static IObservable<T> RepeatAfterDelay<T>(
+			this IObservable<T> source,
+			TimeSpan retryDelay,
+			int repeatCount,
+			IScheduler scheduler)
+		{
+			return RedoAfterDelay(source, retryDelay, repeatCount, scheduler, Observable.Repeat, Observable.Repeat);
+		}
+
+		public static IObservable<T> LogAndRetryAfterDelay<T>(
+			this IObservable<T> source,
+			ILog logger,
+			TimeSpan retryDelay,
+			string message)
+		{
+			return LogAndRetryAfterDelay(source, logger, retryDelay, message, -1, null);
+		}
+		public static IObservable<T> LogAndRetryAfterDelay<T>(
+			this IObservable<T> source,
+			ILog logger,
+			TimeSpan retryDelay,
+			string message,
+			int retryCount,
+			IScheduler scheduler)
+		{
+			var sourceLogged =
+				source
+					.Do(
+						_ => { },
+						ex => logger?.Error(message, ex));
+
+			return RetryAfterDelay(sourceLogged, retryDelay, retryCount, scheduler);
+		}
+
+		private static IObservable<T> RedoAfterDelay<T>(IObservable<T> source, TimeSpan retryDelay, int retryCount, IScheduler scheduler, Func<IObservable<T>, IObservable<T>> reDo,
+			Func<IObservable<T>, int, IObservable<T>> reDoCount)
+		{
+			scheduler = scheduler ?? TaskPoolScheduler.Default;
+			var attempt = 0;
+
+			var deferedObs = Observable.Defer(() => ((++attempt == 1) ? source : source.DelaySubscription(retryDelay, scheduler)));
+			return retryCount > 0 ? reDoCount(deferedObs, retryCount) : reDo(deferedObs);
+		}
+
+		public static IObservable<T> DisposeMany<T>(this IObservable<T> source)
+		{
+			return Observable.Create<T>(obs =>
+			{
+				var serialDisposable = new SerialDisposable();
+				var subscription =
+					source.Subscribe(
+						item =>
+						{
+							serialDisposable.Disposable = item as IDisposable;
+							obs.OnNext(item);
+						},
+						obs.OnError,
+						obs.OnCompleted);
+				return new CompositeDisposable(serialDisposable, subscription);
+			});
+		}
+	}
+}
