b72dcad3b9922728dbdecb23698639bf16cae45c
mdelicati <marco.delicati@evopro-ag.de>
2020-07-01 08:57:22 +0200

    added ResultJson to JsonCommand class

--- EdgeManager.Interfaces/Models/JsonCommand.cs
+++ EdgeManager.Interfaces/Models/JsonCommand.cs
@@ -9,11 +9,12 @@ namespace EdgeManager.Interfaces.Models
     
     public class JsonCommand
     {
-        public JsonCommand(string command)
+        public JsonCommand(string command, string result)
         {
             Command = command;
+            ResultJson = result;
         }
-
         public string Command { get; }
+        public string ResultJson { get; set; }
     }
 }
