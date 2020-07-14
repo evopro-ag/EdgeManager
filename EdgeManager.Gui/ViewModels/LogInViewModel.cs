6863a9d53b5300cb20afd1971a24d7dfea251794
mdelicati <marco.delicati@evopro-ag.de>
2020-07-14 11:48:16 +0200

    added viewmodel for login

---
+++ EdgeManager.Gui/ViewModels/LogInViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+using EdgeManager.Gui.Design;
+using EdgeManager.Interfaces.Extensions;
+using EdgeManager.Interfaces.Services;
+using ReactiveUI;
+
+namespace EdgeManager.Gui.ViewModels
+{
+    public class LogInViewModel : ViewModelBase
+    {
+        private readonly IAzureService azureService;
+        public ReactiveCommand<Unit, Unit> LogInCommand { get; set; }
+        
+
+        public LogInViewModel(IAzureService azureService)
+        {
+            this.azureService = azureService;
+        }
+
+        public override void Initialize()
+        {
+            LogInCommand = ReactiveCommand.CreateFromTask(PerformAzureLogin)
+                    .AddDisposableTo(Disposables)
+                ;
+        }
+
+        private async Task<Unit> PerformAzureLogin()
+        {
+            try
+            {
+                await azureService.Login();
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Error while login", e);
+            }
+
+            return Unit.Default;
+        }
+    }
+
+    internal class DesignLogInViewModel : LogInViewModel
+    {
+        public DesignLogInViewModel() : base(new DesignAzureService())
+        {
+            base.Initialize();
+        }
+    }
+}
