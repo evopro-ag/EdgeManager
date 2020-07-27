f5d5e2de0b81c33a70aaf6ce92fcaa3423694167
Federico Barresi <federico.barresi@evopro-ag.de>
2020-07-27 21:23:44 +0200

    added different icons for different device types
    - moved converters into different files
    - removed console output
    - added static converter to App.xaml

---
+++ EdgeManager.Gui/Converters/BoolToBrushConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace EdgeManager.Gui.Converters
+{
+    public sealed class BoolToBrushConverter : IValueConverter
+    {
+        public BoolToBrushConverter()
+        {
+            this.TrueValue = Brushes.Green;
+            this.FalseValue = Brushes.Gray;
+        }
+
+        public Brush FalseValue { get; set; }
+
+        public Brush TrueValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(value is bool flag) ? DependencyProperty.UnsetValue : flag ? TrueValue : FalseValue;
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
---
+++ EdgeManager.Gui/Converters/ConnectionStateToBrushConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using EdgeManager.Interfaces.Enums;
+
+namespace EdgeManager.Gui.Converters
+{
+    public sealed class ConnectionStateToBrushConverter : IValueConverter
+    {
+        public ConnectionStateToBrushConverter()
+        {
+            this.ConnectedValue = Brushes.Green;
+            this.NotConnectedValue = Brushes.Gray;
+        }
+
+        public Brush NotConnectedValue { get; set; }
+
+        public Brush ConnectedValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(value is ConnectionState flag) ? DependencyProperty.UnsetValue : flag==ConnectionState.Connected ? ConnectedValue : NotConnectedValue;
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
