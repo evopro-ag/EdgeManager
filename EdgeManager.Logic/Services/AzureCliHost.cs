96d3a6a0c66d3729a4b5426cdfb66aa29118379b
Federico Barresi <federico.barresi@evopro-ag.de>
2020-07-30 09:02:33 +0200

    improved performance of checking az cli installed

--- EdgeManager.Logic/Services/AzureCliHost.cs
+++ EdgeManager.Logic/Services/AzureCliHost.cs
@@ -121,10 +121,10 @@ namespace EdgeManager.Logic.Services
 
         public async Task<bool> CheckCli()
         {
-            var result = await powerShell.Execute("az --version");
+            var result = await powerShell.Execute("az version");
             try
             {
-                if (result.First().ToString().Contains("azure-cli")) return true;
+                return result.Any(l => l.ToString().Contains("azure-cli"));
             }
             catch (Exception e)
             {
