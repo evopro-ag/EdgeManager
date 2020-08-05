2fe3948a7eb70d5ed87a3bd6ee6970693ff12898
mdelicati <marco.delicati@evopro-ag.de>
2020-08-05 10:55:03 +0200

    Add ConfirmWindow for delete SelectedDevice

---
+++ EdgeManager.Gui/ViewModels/ConfirmWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using EdgeManager.Interfaces.Extensions;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EdgeManager.Gui.ViewModels
+{
+    public class ConfirmWindowViewModel : ViewModelBase
+    {
+        public override void Initialize()
+        {
+            DeleteCommand = ReactiveCommand.CreateFromTask(DeleteSelectedDevice)
+               .AddDisposableTo(Disposables);
+
+            CloseCommand = ReactiveCommand.CreateFromTask(CloseWindow)
+                .AddDisposableTo(Disposables);
+        }
+
+        public ReactiveCommand<Unit, Unit> DeleteCommand { get; set; }
+        public bool DeleteDevice { get; set; }
+        private Task DeleteSelectedDevice()
+        {
+            DeleteDevice = true;
+            return CloseWindow();
+        }
+
+        public ReactiveCommand<Unit, Unit> CloseCommand { get; set; }
+        private Task CloseWindow()
+        {
+            Window.Close();
+            return Task.FromResult(Unit.Default);
+        }
+
+        public Window Window { get; set; }
+    }
+}
